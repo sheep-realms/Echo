f45490230247361c84f6a11942bbca346a241d70
sheep-realms <sheep_realms@qq.com>
2023-02-19 14:16:48 +0800

    Update doc

---
+++ doc/model_text.md
@@ -0,0 +1,4 @@
+# 范文
+- [多种样式测试](model_text/arknights.js)
+- [中文打字测试](model_text/typewrite.js)
+- [日文打字测试](model_text/nichijoo.js)
---
+++ doc/model_text/typewrite.js
@@ -0,0 +1,64 @@
+echo.send([
+	{
+		text: "zhe'shi",
+		typewrite: true,
+		typewriteResult: '这是'
+	}, {
+		text: "yi'duan",
+		typewrite: true,
+		typewriteResult: '一段'
+	}, {
+		text: "zhong'wen",
+		typewrite: true,
+		typewriteResult: '中文'
+	}, {
+		text: "shu'ru'fa",
+		typewrite: true,
+		typewriteResult: '输入法'
+	}, {
+		text: "da'zi",
+		typewrite: true,
+		typewriteResult: '打字'
+	}, {
+		text: "ce'shi",
+		typewrite: true,
+		typewriteResult: '测试'
+	},
+	'。',
+    {
+		text: "ren'zhi'chu",
+		typewrite: true,
+		typewriteResult: '人之初'
+	}, 
+    '，',
+    {
+		text: "xing'ben'shan",
+		typewrite: true,
+		typewriteResult: '性本善'
+	}, 
+    '。',
+    {
+		text: "j's'b'y",
+		typewrite: true,
+		typewriteResult: '己所不欲'
+	}, 
+    '，',
+    {
+		text: "wu'shi'y'r",
+		typewrite: true,
+		typewriteResult: '勿施于人'
+	}, 
+    '。',
+    {
+		text: "g'l'g'j's's'y",
+		typewrite: true,
+		typewriteResult: '苟利国家生死以'
+	}, 
+    '，',
+    {
+		text: "q'y'h'f'b'q'z",
+		typewrite: true,
+		typewriteResult: '岂因祸福避趋之'
+	}, 
+    '。'
+], {printSpeed:90})
